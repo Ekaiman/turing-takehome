5d9df66097786b5ee10b557207decdbf28da898a
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-06-09 12:19:17 -0600

    Add ability to filter

--- src/Components/ArticleHolder/ArticleHolder.js
+++ src/Components/ArticleHolder/ArticleHolder.js
@@ -1,16 +1,81 @@
-import Article from "../Article/Article"
+import Article from '../Article/Article'
+import { Link } from 'react-router-dom'
+import './ArticleHolder.css'
+import { ArticleDataContext } from '../../Context/ArticleData'
+import { useContext, useState } from 'react'
 
-const ArticleHolder = ({ articles}) => { 
-  const eachArticle = articles.map(article => { 
-    return <Article
-      section={article.section}
-      abstract={article.abstract}
-      byline={article.byline}
-    /> 
+const ArticleHolder = ({}) => {
+  const [filter, setFilter] = useState('')
+
+  const articles = useContext(ArticleDataContext)
+
+  let eachArticle
+
+  eachArticle = articles.map(article => {
+    let { title, section, abstract, byline, id } = article
+
+    return (
+      <Link key={title} to={`article/${id}`}>
+        <Article
+          section={section}
+          title={title}
+          abstract={abstract}
+          byline={byline}
+          key={id}
+        />
+      </Link>
+    )
   })
+
+  let categories = []
+
+  articles.forEach(article => {
+    if (!categories.includes(article.section)) {
+      categories.push(article.section)
+    }
+  })
+
+  const options = categories.sort().map(cat => (
+    <option key={cat} value={cat}>
+      {cat}
+    </option>
+  ))
+
+  if (filter) {
+    eachArticle = articles
+      .filter(article => {
+        if (article.section === filter) {
+          return article
+        }
+      }).map(article => {
+        let { title, section, abstract, byline, id } = article
+
+        return (
+          <Link key={title} to={`article/${id}`}>
+            <Article
+              section={section}
+              title={title}
+              abstract={abstract}
+              byline={byline}
+              key={id}
+            />
+          </Link>
+        )
+      })
+  }
+
   return (
     <div>
-      <p>{eachArticle}</p>
+      <select
+        className='sub-category-option'
+        value={filter}
+        onChange={e => setFilter(e.target.value)}
+      >
+        <option value=''>Show All</option>
+        {options}
+      </select>
+      {console.log(articles)}
+      <section className='article-holder'>{eachArticle}</section>
     </div>
   )
 }
