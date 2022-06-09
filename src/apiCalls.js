b5826538f6e53fba61d3f589a794cb31df53063a
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-06-09 12:17:22 -0600

    Use variable for apiKey

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@ const apiKey = process.env.REACT_APP_API_KEY
 
 const fetchData = () => { 
   return fetch(
-    `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=sxwmQTmsR6jypXRPkGFvplzwCazy3ky9`
+    `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${apiKey}`
   ).then(res => res.json())
 }
 
