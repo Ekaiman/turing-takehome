40f2bcdd02e1f18bfbb7e58c24542ba52a9cc449
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-06-10 10:51:49 -0600

    Add image and change styling

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,26 +1,35 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { ArticleDataContext } from '../../Context/ArticleData'
 import { useContext } from 'react'
+import './Details.css'
 
 const Deatils = ({}) => {
   const { id } = useParams()
   const navigate = useNavigate()
   const articles = useContext(ArticleDataContext)
 
-  
-  let { title, url, published_date, updated_date, byline } = articles[id]
+  let {
+    title,
+    url,
+    published_date,
+    updated_date,
+    byline,
+    abstract,
+    multimedia
+  } = articles[id]
 
   return (
-    <div>
+    <div className='detail-holder'>
+      <button onClick={() => navigate(-1)}>Back</button>
       <h1>{title}</h1>
+      <p>{byline}</p>
+      <p>{abstract}</p>
       <a href={url}>Article Link</a>
       <p>Published: {published_date}</p>
-      {published_date !== updated_date && (
-        <p>Last Updated:{updated_date}</p>
-      )}
-      <p>{byline}</p>
-      {/* {console.log(articles)} */}
-      <button onClick={() => navigate(-1)}>Back</button>
+      {published_date !== updated_date && <p>Last Updated:{updated_date}</p>}
+      <div className='img-holder'>
+        <img className="image" src={multimedia[0].url} />
+      </div>
     </div>
   )
 }
